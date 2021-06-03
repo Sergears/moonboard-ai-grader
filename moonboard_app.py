6d8eb2094e975473c8ed186aadd07e24ed6bda15
Sergears <58189334+Sergears@users.noreply.github.com>
2021-06-03 19:51:03 +0200

    Add files via upload

---
+++ moonboard_app.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 11:34:12 2021
+
+@author: sa262627
+"""
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import colors
+from matplotlib.ticker import MultipleLocator
+import matplotlib.image as mpimg
+import matplotlib.gridspec as gridspec
+import pickle
+import my_neural_regressor
+import my_neural_classifier
+import os
+
+
+
+
+st.title("Moonboard AI grader")
+st.sidebar.header('User input parameters')
+
+st.markdown('Moonboard is a standartised training wall for rock climbers. A boulder problem is created by choosing the allowed holds. It then gets graded and repeated by users worldwide.')
+st.markdown('In 2017 setup, grades of diffuculcy range from 6a+ to 8b+. Use the sidebar to select a problem from the test set. The problem will be automatically graded by the already trained neural networks.')
+
+
+@st.cache
+def load_data(path, nrows=0):    
+    if nrows == 0:
+        data = pd.read_csv(path + '/2017_test_data.csv')
+    else:
+        data = pd.read_csv(path + '/2017_test_data.csv', nrows=nrows)
+    return data
+
+
+def user_input_features(data):
+    """    
+
+    Parameters
+    ----------
+    data : pandas dataframe
+        problem data of shape (n_problems, 199) organized as an array of 
+        hold_statuses (len 198) and user_grade (len 1)
+
+    Returns
+    -------
+    hold_statuses : numpy array of (198, 1)
+        hold_statuses for the selected problem.
+    user_grade : int
+        problem user grade.
+
+    """
+    
+    # grade selector
+    grade_names = ['all', '6a+', '6b', '6b+', '6c', '6c+', '7a', '7a+', 
+                       '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b', '8b+']
+    grade_name = st.sidebar.select_slider('Difficulcy grade', grade_names)
+    if grade_name == 'all':
+        grade = -1
+    else:
+        grade = grade_names.index(grade_name) + 1
+    if grade != -1:
+        data = data[data['Grade'] == grade]
+        
+    # repeats selector
+    max_repeats = max(data['Repeats'])
+    min_repeats = st.sidebar.slider('Show only problems with repeats above', min_value=0, max_value=max_repeats, value=0)
+    data = data[data['Repeats'] >= min_repeats]
+    
+    # select problem
+    if len(data) > 1:
+        problem_ind = st.sidebar.slider('Problem ind', min_value=0, max_value=len(data)-1, value=0)
+    else:
+        problem_ind = 0
+    row = data.iloc[problem_ind]
+    hold_statuses = np.array(row[0:198])
+    hold_statuses = hold_statuses.reshape(198, 1)
+    user_grade = int(row[-1])
+    
+    return hold_statuses, user_grade
+
+
+def display_problem(path, hold_statuses, user_grade, forest_grade, NN_regr_grade, NN_class_probs):
+    """
+    Displays hold selection overlayed on the moonboard setup
+
+    Parameters
+    ----------
+    hold_statuses : dataframe column of shape (198,1)
+        array of holds in the problem: 1 if a hold is in the problem, 0 otherwise.
+    user_grade : integer from 0 to 16
+        user grade (ground truth) of the problem
+    forest_grade : float
+        grade prediction by the random forest model.
+    NN_regr_grade : float
+        grade prediction by the neural network regressor.
+    NN_class_probs : float
+        grade prediction by the neural network classifier.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    # convert hold_statuses to a 2D array (field) to be dispayed
+    n_hold_rows = 18
+    n_hold_cols = 11
+    hold_letters = 'ABCDEFGHIJK'
+    hold_heights = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', 
+                    '12', '13', '14', '15', '16', '17', '18']
+    field = [[0 for _ in range(n_hold_cols)] for _ in range(n_hold_rows)]
+    for i, hold_status in enumerate(hold_statuses):
+        hold_row = i % n_hold_rows
+        hold_col = (i - hold_row) // n_hold_rows
+        if hold_status == 1:
+            field[hold_row][hold_col] = 1
+
+    # Create grid sub plots
+    gs = gridspec.GridSpec(5, 2)
+    gs.update(wspace=0.4, hspace=0.1)
+    fig = plt.figure()  
+    
+    # moonboard setup picture
+    ax_problem = plt.subplot(gs[:, 0])
+    ax_problem.set_title('Selected problem')
+    img = mpimg.imread(path + '/moon_setup_2017_edit.jpg')
+    ax_problem.imshow(img, extent=[-0.5, 10.5, -0.5, 17.5])
+    
+    # overlay hold positions
+    cmap = colors.ListedColormap(['white', 'blue'])
+    bounds = [0, 0.5, 1]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    ax_problem.imshow(field, cmap=cmap, norm=norm, origin='lower', alpha=0.5)
+    ax_problem.set_xticks([i for i in range(n_hold_cols)])
+    ax_problem.set_xticklabels(hold_letters)
+    ax_problem.set_yticks([i for i in range(n_hold_rows)])
+    ax_problem.set_yticklabels(hold_heights)
+    spacing = 0.5
+    minorLocator = MultipleLocator(spacing)
+    minorLocator2 = MultipleLocator(spacing)
+    ax_problem.yaxis.set_minor_locator(minorLocator)
+    ax_problem.xaxis.set_minor_locator(minorLocator2)
+    ax_problem.grid(which='minor')
+    
+    # vertical bar chart with ground truth and regressor predictions
+    ax_regressors = plt.subplot(gs[0:2, 1])
+    ax_regressors.set_title('AI grade prediction')    
+    regressor_models = ['NN \n regressor', 'Random \n forest', 'Ground \n truth']
+    grade_predictions = [NN_regr_grade, forest_grade, user_grade]
+    x_tick_pos = [i for i in range(17)]
+    y_tick_pos = [i for i, _ in enumerate(regressor_models)]
+    grade_names_full = ['5+', '6a', '6a+', '6b', '6b+', '6c', '6c+', '7a', '7a+', 
+                   '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b', '8b+']    
+    ax_regressors.barh(y_tick_pos, grade_predictions, color='r', height=0.2)    
+    ax_regressors.set_yticks(y_tick_pos)
+    ax_regressors.set_yticklabels(regressor_models)
+    ax_regressors.set_xticks(x_tick_pos)
+    ax_regressors.set_xticklabels([])
+    ax_regressors.grid(axis='x')
+    ax_regressors.set_xlim([0, 16.5])
+    
+    # horizontal bar chart with classifier prediction
+    ax_classifier = plt.subplot(gs[2, 1])
+    predicted_distr = NN_class_probs
+    ax_classifier.bar(x_tick_pos, predicted_distr, color='r', width=0.8)
+    ax_classifier.set_xlim([0, 16.5])
+    ax_classifier.set_xticks(x_tick_pos)
+    ax_classifier.set_xticklabels(grade_names_full)
+    for label in ax_classifier.xaxis.get_ticklabels()[::2]:
+        label.set_visible(False)
+    ax_classifier.set_yticks([])
+    ax_classifier.set_ylabel('NN \n classifier', rotation=0, labelpad=30)
+    ax_classifier.grid(axis='x')    
+    
+    fig.tight_layout()    
+    st.pyplot(fig)
+
+
+def print_grade_predictions(user_grade, forest_grade, NN_regr_grade, NN_class_probs):
+    """
+    print grade predictions in text (predictions are rounded to the nearest grade)
+
+    Parameters
+    ----------
+    user_grade : integer from 0 to 16
+        user grade (ground truth) of the problem
+    forest_grade : float
+        grade prediction by the random forest model.
+    NN_regr_grade : float
+        grade prediction by the neural network regressor.
+    NN_class_probs : float
+        grade prediction by the neural network classifier.
+
+    Returns
+    -------
+    None.
+
+    """
+    grade_names_full = ['5+', '6a', '6A+', '6B', '6B+', '6C', '6C+', '7A', 
+                       '7A+', '7B', '7B+', '7C', '7C+', '8A', '8A+', '8B', '8B+']  
+    'User grade (ground truth):', grade_names_full[int(user_grade)]
+    
+    forest_grade_rounded = round(forest_grade)
+    forest_grade_rounded = min(forest_grade_rounded, len(grade_names_full) - 1)
+    forest_grade_rounded = max(0, forest_grade_rounded)
+    'Random forest prediction:', grade_names_full[forest_grade_rounded]
+        
+    NN_regr_grade_rounded = round(NN_regr_grade)
+    NN_regr_grade_rounded = min(NN_regr_grade_rounded, len(grade_names_full) - 1)
+    NN_regr_grade_rounded = max(0, NN_regr_grade_rounded)
+    'Neural network regressor prediction', grade_names_full[NN_regr_grade_rounded]
+    
+    NN_class_grade = np.argmax(NN_class_probs)
+    'Neural network classifier prediction', grade_names_full[NN_class_grade]
+
+
+def conver_normalized_grade_to_abs_grade(normalized_grade):
+    """   
+    Parameters
+    ----------
+    normalized_grade : float
+        grade vaule compressed into the interval [0.25, 0.75] used for network learning
+
+    Returns
+    -------
+    abs_grade: float
+        absolute grade in the interval [0, 16].
+
+    """
+    abs_grade = (normalized_grade - 0.25) * 16 / 0.5
+    return abs_grade
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+data = load_data(path)
+hold_statuses, user_grade = user_input_features(data)
+
+# predict grade with random forest from sklearn
+net_name = '/forest_model'
+with open(path + net_name + '.pkl', 'rb') as input:
+    forest_model = pickle.load(input)
+normalized_grade = forest_model.predict(hold_statuses.T)[0]
+forest_grade = conver_normalized_grade_to_abs_grade(normalized_grade)
+
+# predict grade with my neural network regressor
+net_name = '/nn_regressor_network'
+file = open(path + net_name + '.pkl', 'rb')
+parameters = pickle.load(file)
+AL, caches = my_neural_regressor.L_model_forward(hold_statuses, parameters, 'relu', 'relu')
+normalized_grade = AL[0,0]
+NN_regr_grade = conver_normalized_grade_to_abs_grade(normalized_grade)
+
+# predict grade with my neural network regressor
+net_name = '/nn_classifier_network'
+file = open(path + net_name + '.pkl', 'rb')
+parameters = pickle.load(file)
+AL, caches = my_neural_classifier.L_model_forward(hold_statuses, parameters, 'relu', 'sigmoid')
+output = AL.T[0]
+output_sum = sum(output)
+NN_class_probs = np.array(output / output_sum)
+
+display_problem(path, hold_statuses, user_grade, forest_grade, NN_regr_grade, NN_class_probs)
+print_grade_predictions(user_grade, forest_grade, NN_regr_grade, NN_class_probs)
---
+++ my_neural_classifier.py
@@ -0,0 +1,445 @@
+"""
+architecture: n layers of relu, output layer of 17 sigmoids
+to do: create functions:
+model(hyerparams)
+forward
+backward
+"""
+import numpy as np
+
+
+def sigmoid(Z):
+    """ returns activation and cache containing Z"""
+    return 1 / (1 + np.exp(-Z)), Z
+
+
+def relu(Z):
+    """ returns activation and cache containing Z"""
+    cache = Z
+    Z[Z < 0] = 0
+    return Z, cache
+
+
+def relu_backward(dA, Z):
+    """ used as dZ = relu_backward(dA, activation_cache)"""
+    Z[Z <= 0] = 0
+    Z[Z > 0] = 1
+    return dA * Z
+
+
+def sigmoid_backward(dA, Z):
+    """ used as dZ = sigmoid_backward(dA, activation_cache)"""
+    return np.exp(-Z) / (1 + np.exp(-Z)) ** 2 * dA
+
+
+def initialize_parameters_deep(layer_dims, w_init):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * w_init
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value
+    cache -- a python tuple containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+             cache consists of (A_prev, W, b) and Z
+    """
+
+    if activation == "sigmoid":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        Z, linear_cache = np.dot(W, A_prev) + b, (A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        Z, linear_cache = np.dot(W, A_prev) + b, (A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+
+def L_model_forward(X, parameters, act, last_act):
+    """
+    Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
+
+    Arguments:
+    X -- data, numpy array of shape (input size, number of examples)
+    parameters -- output of initialize_parameters_deep()
+
+    Returns:
+    AL -- last post-activation value
+    caches -- list of caches containing:
+                every cache of linear_activation_forward() (there are L-1 of them, indexed from 0 to L-1)
+    """
+
+    caches = []
+    A = X
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    for l in range(1, L):
+        A_prev = A
+        W, b = parameters['W' + str(l)], parameters['b' + str(l)]
+        if act == 'sigmoid':
+            A, cache = linear_activation_forward(A_prev, W, b, "sigmoid")
+        elif act == 'relu':
+            A, cache = linear_activation_forward(A_prev, W, b, "relu")
+
+        caches.append(cache)
+
+    W, b = parameters['W' + str(L)], parameters['b' + str(L)]
+    if last_act == 'sigmoid':
+        AL, cache = linear_activation_forward(A, W, b, "sigmoid")
+    elif last_act == 'relu':
+        AL, cache = linear_activation_forward(A, W, b, "relu")
+    caches.append(cache)
+
+    return AL, caches
+
+
+def compute_cost(AL, Y, cost_func, lambd, parameters):
+    """
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (17, number of examples)
+    Y -- true "label" vector (containing 0 if not the grade, 1 if the grade), shape (17, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+    m = Y.shape[1]
+
+    cost = 0
+    for i in range(m):
+        y = Y[:,i]
+        a = AL[:,i]
+        if cost_func == 'cross_entropy':
+            cost += - 1 / m * (np.dot(y.T, np.log(a)) + np.dot((1 - y).T, np.log(1 - a)))  # can I vectorize acrooss samples?
+        elif cost_func == 'quadratic':
+            cost += 1 / 2 / m * np.dot((y - a).T, (y - a))
+
+    # add weight decay term
+    L = len(parameters) // 2  # number of layers in the neural network
+    for l in range(L):
+        w = parameters["W" + str(l + 1)]
+        cost += lambd / 2 / m * np.sum(w ** 2)
+
+    return cost
+
+
+def compute_n_correct(AL, Y):
+    """
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (17, number of examples)
+    Y -- true "label" vector (containing 0 if not the grade, 1 if the grade), shape (17, number of examples)
+
+    Returns:
+    n_correct - number of correctly predicted grades
+    """
+
+    m = Y.shape[1]
+    n_correct = 0
+    error_sum = 0
+    for i in range(m):
+        y = Y[:,i]
+        a = AL[:,i]
+        predicted_grade = np.argmax(a)
+        correct_grade = np.argmax(y)
+        n_correct += int(predicted_grade == correct_grade)
+        error_sum += np.abs(predicted_grade - correct_grade)
+    mae = error_sum / m  # mean squared error
+    return n_correct, mae
+
+
+def linear_backward(dZ, cache):
+    """
+    Implement the linear portion of backward propagation for a single layer (layer l)
+
+    Arguments:
+    dZ -- Gradient of the cost with respect to the linear output (of current layer l)
+    cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1 / m * np.dot(dZ, A_prev.T)
+    db = 1 / m * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+
+def linear_activation_backward(dA, cache, activation):
+    """
+    Implement the backward propagation for the LINEAR->ACTIVATION layer.
+
+    Arguments:
+    dA -- post-activation gradient for current layer l
+    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+
+def L_model_backward(AL, Y, caches, cost_func, act, last_act):
+    """
+    Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
+
+    Arguments:
+    AL -- probability vector, output of the forward propagation (L_model_forward())
+    Y -- true "label" vector (containing 0 if not the grade, 1 if the grade)
+    caches -- list of caches containing:
+                every cache of linear_activation_forward() with "relu" (it's caches[l], for l in range(L-1) i.e l = 0...L-2)
+                the cache of linear_activation_forward() with "sigmoid" (it's caches[L-1])
+
+    Returns:
+    grads -- A dictionary with the gradients
+             grads["dA" + str(l)] = ...
+             grads["dW" + str(l)] = ...
+             grads["db" + str(l)] = ...
+    """
+    grads = {}
+    L = len(caches)  # the number of layers
+    Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
+
+    # Initializing the backpropagation
+    if cost_func == 'cross_entropy':
+        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))  # for cross-entropy cost function
+    elif cost_func == 'quadratic':
+        dAL = AL - Y  # for quadratic cost function
+
+    # Lth layer. Inputs: "dAL, current_cache". Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
+    current_cache = caches[L - 1]
+    if last_act == 'sigmoid':
+        grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL,
+                                                                                                      current_cache,
+                                                                                                      "sigmoid")
+    elif last_act == 'relu':
+        grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL,
+                                                                                                          current_cache,
+                                                                                                          "relu")
+
+    # Loop from l=L-2 to l=0
+    for l in reversed(range(L - 1)):
+        # Inputs: "grads["dA" + str(l + 1)], current_cache". Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)]
+        current_cache = caches[l]
+        if act == 'sigmoid':
+            dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache,
+                                                                        "sigmoid")
+        elif act == 'relu':
+            dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache, "relu")
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+
+def update_parameters(parameters, grads, eta, lambd, m):
+    """
+    Update parameters using gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters
+    grads -- python dictionary containing your gradients, output of L_model_backward
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+                  parameters["W" + str(l)] = ...
+                  parameters["b" + str(l)] = ...
+    """
+
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    # Update rule for each parameter. Use a for loop.
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] * (1 - eta * lambd / m) - eta * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - eta * grads["db" + str(l + 1)]
+    return parameters
+
+
+def train_model(X, Y, X_test, Y_test, layers_dims, mini_batch_size, eta=0.1, n_epochs=30, cost_func = 'cross_entropy', act='sigmoid', last_act='sigmoid', w_init=1.0, lambd=0):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+
+    Arguments:
+    X -- data, numpy array of shape (n_data, number of examples)
+    Y -- true "label" vector (containing 0 if not the actual grade, 1 if actual grade), of shape (17, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    eta -- learning rate of the gradient descent update rule
+    n_epochs -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    parameters = initialize_parameters_deep(layers_dims, w_init)
+
+    m = X.shape[1]
+
+    # grad check
+    X_temp = np.random.rand(X.shape[0], 1)
+    Y_temp = np.random.rand(Y.shape[0], 1)
+    AL, caches = L_model_forward(X_temp, parameters, act, last_act)
+    grads = L_model_backward(AL, Y_temp, caches, cost_func, act, last_act)
+    approx_grads = find_approx_gradients(parameters, X_temp, Y_temp, act, last_act, cost_func, lambd)
+
+    flat_grads = np.array([])
+    flat_grads_approx = np.array([])
+    L = len(parameters) // 2  # number of layers in the neural network
+    for l in range(L):
+        dW = grads["dW" + str(l + 1)]
+        db = grads["db" + str(l + 1)]
+        flat_grads = np.append(flat_grads, dW.flatten())
+        flat_grads = np.append(flat_grads, db.flatten())
+
+        dW_appr = approx_grads["dW" + str(l + 1)]
+        db_appr = approx_grads["db" + str(l + 1)]
+        flat_grads_approx = np.append(flat_grads_approx, dW_appr.flatten())
+        flat_grads_approx = np.append(flat_grads_approx, db_appr.flatten())
+
+    err = flat_grads - flat_grads_approx
+    result = np.linalg.norm(err) / (np.linalg.norm(flat_grads) + np.linalg.norm(flat_grads_approx))
+    print('grad check result:', result)
+    print(flat_grads[0:5], flat_grads_approx[0:5])
+
+
+    # Loop (gradient descent)
+    for i in range(0, n_epochs):
+
+        permutation = np.random.permutation(m)
+        X = X[:, permutation]
+        Y = Y[:, permutation]
+        n_minibatches = int(m / mini_batch_size)
+
+        for i_mini in range(n_minibatches):
+            X_mini = X[:, i_mini*mini_batch_size : (i_mini+1)*mini_batch_size]
+            Y_mini = Y[:, i_mini * mini_batch_size: (i_mini + 1) * mini_batch_size]
+
+            # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+            AL, caches = L_model_forward(X_mini, parameters, act, last_act)
+
+            # Backward propagation.
+            grads = L_model_backward(AL, Y_mini, caches, cost_func, act, last_act)
+
+            # Update parameters.
+            parameters = update_parameters(parameters, grads, eta, lambd, mini_batch_size)
+
+        # Compute cost and estamate performance
+        if i % 10 == 0:
+            AL_full, caches_full = L_model_forward(X, parameters, act, last_act)  # compute for all samples (not mini-batch)
+            cost = compute_cost(AL_full, Y, cost_func, lambd, parameters)
+            n_correct_training, mae_training = compute_n_correct(AL_full, Y)
+            training_result = str(n_correct_training) + ' / ' + str(m) + ' correct, mae = ' + "{:.2f}".format(mae_training)
+
+            m_test = X_test.shape[1]
+            AL_test, caches_test = L_model_forward(X_test, parameters, act, last_act)
+            n_correct_test, mae_test = compute_n_correct(AL_test, Y_test)
+            test_result = str(n_correct_test) + ' / ' + str(m_test) + ' correct, mae = ' + "{:.2f}".format(mae_test)
+
+            print('epoch:', i, ', cost:', "{:.3f}".format(cost), 'training preformance:', training_result, 'test preformance:', test_result)
+
+    return parameters
+
+
+def find_approx_gradients(parameters, X, Y, act, last_act, cost_func, lambd):
+    eps = 1e-7
+    grads_approx = {}
+    L = len(parameters) // 2  # number of layers in the neural network
+    for l in range(L):
+        W = parameters["W" + str(l + 1)]
+        grads_approx["dW" + str(l + 1)] = np.zeros_like(W)
+        for i in range(W.shape[0]):
+            for j in range(W.shape[1]):
+                W_new = np.copy(W)
+
+                W_new[i,j] -= eps
+                params_new = parameters.copy()
+                params_new["W" + str(l + 1)] = W_new
+                AL_full, caches_full = L_model_forward(X, params_new, act, last_act)
+                cost_minus = compute_cost(AL_full, Y, cost_func, lambd, parameters)
+
+                W_new[i, j] += 2 * eps
+                params_new = parameters.copy()
+                params_new["W" + str(l + 1)] = W_new
+                AL_full, caches_full = L_model_forward(X, params_new, act, last_act)
+                cost_plus = compute_cost(AL_full, Y, cost_func, lambd, parameters)
+
+                grads_approx["dW" + str(l + 1)][i,j] = (cost_plus - cost_minus) / 2 / eps
+
+        B = parameters["b" + str(l + 1)]
+        grads_approx["db" + str(l + 1)] = np.zeros_like(B)
+        for i in range(B.shape[0]):
+            B_new = np.copy(B)
+
+            B_new[i] -= eps
+            params_new = parameters.copy()
+            params_new["b" + str(l + 1)] = B_new
+            AL_full, caches_full = L_model_forward(X, params_new, act, last_act)
+            cost_minus = compute_cost(AL_full, Y, cost_func, lambd, parameters)
+
+            B_new[i] += 2 * eps
+            params_new = parameters.copy()
+            params_new["b" + str(l + 1)] = B_new
+            AL_full, caches_full = L_model_forward(X, params_new, act, last_act)
+            cost_plus = compute_cost(AL_full, Y, cost_func, lambd, parameters)
+
+            grads_approx["db" + str(l + 1)][i,0] = (cost_plus - cost_minus) / 2 / eps
+
+    return grads_approx
+
+
